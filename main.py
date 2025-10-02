d6968ca9c1c95b0eda9aaaf1ab7bf508db79e371
Danil <karatelanalov@yandex.ru>
2025-10-02 20:21:00 +0300

    added nvidia and upgrade main.py

--- main.py
+++ main.py
@@ -7,15 +7,25 @@ app = FastAPI(title="LLM", debug=True)
 
 
 @app.post(
-    path="/no_streaming_response",
+    path="/no-streaming-response",
     summary="sample response without streaming",
     response_model=LLMResponse,
     tags=["Sample Response"],
+    operation_id="sample-no-streaming-response",
+    description="""
+
+| Параметр | Тип Данных | Описание                                                                             |
+| -------- | ---------- | ------------------------------------------------------------------------------------ |
+| `model`  | str        | название и версия LLM-модели, которую нужно использовать для генерации               |
+| `prompt` | str        | текстовый запрос или вопрос, который будет отправлен модели                          |
+| `stream` | str        | флаг, указывающий, нужно ли возвращать ответ потоково (`true`) или целиком (`false`) |
+
+""",
 )
 async def no_streaming_response(data: Prompt):
     async with httpx.AsyncClient(timeout=None) as client:
         res = await client.post(
-            url="http://localhost:11434/api/generate",
+            url="http://ollama:11434/api/generate",
             json=data.model_dump(),
         )
 
